e53025d48b3a4cc2d5b25c13836c4cb69b5a9048
Clint Milner <clinton.milner@rebasoft.net>
2019-10-15 22:00:45 +0100

    Updates

---
+++ precache-manifest.713ca7466da57fdec198fe01ee2a8f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108dc878d3c14f0911681c58a9863e29",
+    "url": "/twitter-stats/index.html"
+  },
+  {
+    "revision": "5190a45994ca9115706c",
+    "url": "/twitter-stats/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4418cb441b8681a8aa09",
+    "url": "/twitter-stats/static/css/main.c248e953.chunk.css"
+  },
+  {
+    "revision": "5190a45994ca9115706c",
+    "url": "/twitter-stats/static/js/2.08705f6c.chunk.js"
+  },
+  {
+    "revision": "4418cb441b8681a8aa09",
+    "url": "/twitter-stats/static/js/main.26b55616.chunk.js"
+  },
+  {
+    "revision": "005af659a82b828be2b9",
+    "url": "/twitter-stats/static/js/runtime-main.8f7b521f.js"
+  }
+]);
